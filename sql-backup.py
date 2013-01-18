bc72d24bdb947be8d70ebcb98b0633b0552b1cf5
Alexander Chernyakhovsky <achernya@mit.edu>
2013-01-17 20:07:47 -0500

    Initial version of sql backup script

---
+++ sql-backup.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013 Alexander Chernyakhovsky
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import json
+import logging
+import sys
+import subprocess
+from datetime import date
+from optparse import OptionParser
+
+# Configuration has to be stored in a global variable as it has to be
+# accessible to all functions
+config = None
+
+def main():
+    parser = OptionParser()
+    parser.add_option('-c', '--config', dest='config',
+                      help='path to configuration', metavar='FILE')
+    parser.add_option('-v', '--verbose',
+                      action='store_true', dest='verbose', default=False,
+                      help='print status messages to stdout')
+    (options, args) = parser.parse_args()
+
+    # Uncomment this to make -v actually work
+    #logger.setLevel(logging.INFO if options.verbose else 
+    #                logging.WARNING)
+
+    logger.debug(options)
+
+    if options.config is None:
+        die('No configuration file specified')
+    load_config(options.config)
+
+    logger.debug(config)
+
+    result_file = get_backup_file_path()
+    logger.info('Backing up to "%s"' % (result_file,))
+    perform_backup(result_file)
+
+def setup_logger():
+    logger = logging.getLogger('sql-backup')
+    logger.setLevel(logging.DEBUG)
+    
+    # Create console handler and formatter
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+
+    # Add ch to logger
+    logger.addHandler(ch)
+
+    return logger
+
+def load_config(filename):
+    global config
+    try:
+        config = json.load(open(filename, 'r'))
+    except:
+        logger.exception('Failed to load configuration file "%s"' % (filename,))
+
+    logger.info('Loaded "%s"' % (filename,))
+
+def get_backup_dir(backup_type):
+    return "%s/%s" % (config['backup_path'], backup_type)
+
+def get_backup_file_path(backup_type='daily'):
+    subconfig = config['naming_scheme']
+    now = date.today()
+    return "%s/%s%s%s" % (get_backup_dir(backup_type), subconfig['prefix'], 
+                          now.strftime(subconfig['date_format']), subconfig['suffix'])
+
+def perform_backup(filename):
+    mysqldump = log_and_popen(config['dumper'], stdout=subprocess.PIPE)
+    compressor = log_and_popen(config['compressor'], stdin=mysqldump.stdout, stdout=open(filename, 'wb'))
+    mysqldump.stdout.close()
+    compressor.wait()
+
+def log_and_popen(*args, **kwargs):
+    logger.debug('Popen(%r, %r)' % (args, kwargs))
+    return subprocess.Popen(*args, **kwargs)
+
+def die(error):
+    logger.error(error)
+    sys.exit(1)
+
+logger = setup_logger()
+
+if __name__ == '__main__':
+    main()
