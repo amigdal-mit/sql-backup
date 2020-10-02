1dba8efc880c85bc6584fc88b64073dbf8e3be73
Quentin Smith <quentin@mit.edu>
2020-10-02 01:14:56 -0400

    Backup databases in ascending order

--- mysqlshow.py
+++ mysqlshow.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 db = MySQLdb.connect(read_default_file='~/.my.cnf')
 cursor = db.cursor()
-cursor.execute('SELECT SCHEMA_NAME AS `Database`, cast(CONVERT(SCHEMA_NAME USING filename) as binary) AS `Filename`  FROM INFORMATION_SCHEMA.SCHEMATA')
+cursor.execute('SELECT SCHEMA_NAME, cast(CONVERT(SCHEMA_NAME USING filename) as binary) FROM INFORMATION_SCHEMA.SCHEMATA ORDER BY SCHEMA_NAME ASC')
 for (database, filename) in cursor:
     print(database, end='\0')
     print(filename, end='\0')
